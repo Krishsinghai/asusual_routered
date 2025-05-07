62af24afb2a2977b2f839672c90e871c5f0e87f0
Krish Singhai <singhaikrish769@gmail.com>
2025-05-08 00:01:09 +0530

    footer content update

--- AsUsual/models/images.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const imageSchema = new mongoose.Schema({
-    name: String,
-    data: String, // Store the Base64 encoded string here
-});
-
-module.exports = mongoose.model('Image', imageSchema);
